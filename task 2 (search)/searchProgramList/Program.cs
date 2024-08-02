ea772d4a1b12467784c7d94666db9782a5737df9
M0uradMohamed <mouradmoahmed@gmail.com>
2024-08-02 22:24:17 +0300

    first commit

---
+++ task 2 (search)/searchProgramList/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace searchProgramList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            bool going = true;
+            do
+            {
+                
+                try
+                {
+                    Console.Write("Enter a number to add : ");
+                    int number = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers[i] == number)
+                            throw new Exception("this number already in the list");
+                    }
+                    numbers.Add(number);
+                }
+                catch(FormatException exe) 
+                {
+                    Console.WriteLine($"input is not an intger ");
+                }
+                catch( Exception exe)
+                {
+                    Console.WriteLine(exe.Message);
+                }
+
+            }while (going);
+        }
+    }
+}
